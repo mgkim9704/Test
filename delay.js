d797ea2f45aee41a259fd0704e4ad0d4014b2ba6
mgkim9704 <mgkim9704@kaist.ac.kr>
2016-11-25 00:45:11 +0900

    Update delay.js

--- delay.js
+++ delay.js
@@ -1,62 +1,55 @@
-var Voice = function(context, parameters, fx_input)
-
-
-
-	this.context = context;
-	this.input = context.createGain();
-
-	// create nodes
-	this.delayLine = context.createDelay();
-	this.feedbackGain = context.createGain();
-	this.wetGain = context.createGain(); 
-	this.dryGain = context.createGain();
-
-	// connect 
-	this.input.connect(this.delayLine);
-	this.delayLine.connect(this.feedbackGain);
-	this.feedbackGain.connect(this.wetGain);
-	this.feedbackGain.connect(this.delayLine);
-
-	this.input.connect(this.dryGain);
-
-	this.dryGain.connect(fx_input);
-	this.wetGain.connect(fx_input);
-
-	this.delayLine.delayTime.value = parameters.delayTime;
-	this.feedbackGain.gain.value = parameters.delayFeedbackGain;
-
-	this.wetGain.gain.value = parameters.delayWetDry;
-	this.dryGain.gain.value = (1-parameters.delayWetDry);
-
-	this.parameters = parameters;
-}
-
-var Delay = function(context, parameters) {
-	this.context = context;
-	this.parameters = parameters;
-	Voice(context, parameters, this.fx_input);
-}
-
-Delay.prototype.updateParams = function (params, value) {
-
-	switch (params) {
-		case 'delay_time': 
-			this.parameters.delayTime = value;
-			this.delayLine.delayTime.value = value;
-			break;		
-		case 'delay_feedback_gain': 
-			this.parameters.delayFeedbackGain = value;
-			this.feedbackGain.gain.value = value;
-			break;		
-		case 'delay_dry_wet':
-			this.parameters.delayWetDry = value;
-			this.wetGain.gain.value = value;
-			this.dryGain.gain.value = 1 - value;
-			break;		
-	}
-}
-
-
-Delay.prototype.connect = function(node) {
-	this.fx_input = node.input;
-}
+ var Delay = function(context, parameters) {
+ 
+ 	this.context = context;
+ 	this.input = context.createGain();
+ 
+ 	// create nodes
+ 	this.delayLine = context.createDelay();
+ 	this.feedbackGain = context.createGain();
+ 	this.wetGain = context.createGain(); 
+ 	this.dryGain = context.createGain();
+ 
+ 	// connect 
+ 	this.input.connect(this.delayLine);
+ 	this.delayLine.connect(this.feedbackGain);
+ 	this.feedbackGain.connect(this.wetGain);
+ 	this.feedbackGain.connect(this.delayLine);
+ 
+ 	this.input.connect(this.dryGain);
+ 
+ 	this.dryGain.connect(this.fx_input);
+ 	this.wetGain.connect(this.fx_input);
+ 
+ 	this.delayLine.delayTime.value = parameters.delayTime;
+ 	this.feedbackGain.gain.value = parameters.delayFeedbackGain;
+ 
+ 	this.wetGain.gain.value = parameters.delayWetDry;
+ 	this.dryGain.gain.value = (1-parameters.delayWetDry);
+ 
+ 	this.parameters = parameters;
+ }
+ 
+ 
+ Delay.prototype.updateParams = function (params, value) {
+ 
+ 	switch (params) {
+ 		case 'delay_time': 
+ 			this.parameters.delayTime = value;
+ 			this.delayLine.delayTime.value = value;
+ 			break;		
+ 		case 'delay_feedback_gain': 
+ 			this.parameters.delayFeedbackGain = value;
+ 			this.feedbackGain.gain.value = value;
+ 			break;		
+ 		case 'delay_dry_wet':
+ 			this.parameters.delayWetDry = value;
+ 			this.wetGain.gain.value = value;
+ 			this.dryGain.gain.value = 1 - value;
+ 			break;		
+ 	}
+  }
+  
+  
+ Delay.prototype.connect = function(node) {
+ 	this.fx_input = node.input;
+ }
