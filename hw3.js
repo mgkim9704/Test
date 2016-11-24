03d015d8a0ab306eb2e99625faa4a550a5beed03
mgkim9704 <mgkim9704@kaist.ac.kr>
2016-11-24 18:33:36 +0900

    Update hw3.js

--- hw3.js
+++ hw3.js
@@ -131,7 +131,8 @@ nx.onload = function() {
 	gui_reverb_wet_dry.max = 1;
 		gui_reverb_wet_dry.set({ value: reverb_params.reverbWetDry })
 	gui_reverb_wet_dry.on('*',function(data) {
-		delay.updateParams('reverb_dry_wet', data.value);
+		reverb.updateParams('reverb_dry_wet', data.value);
+		reverb.loadIumpulseResponse();
 	});
 
 	// Keyboard 	
